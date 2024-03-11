217251d7b8ad0c6758ba3051c4d28c2d32fb0984
Shivam Padaliya <98576767+shivam-65@users.noreply.github.com>
2024-03-11 15:24:00 +0530

    create post component
    add delete functionality in post

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -6,26 +6,29 @@ import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 import { useState } from "react";
+import PostListProvider from "./store/post-list-store";
 
 function App() {
   const [selectedTab, setselectedTab] = useState("Home");
 
   return (
-    <div className="app-container">
-      <Sidebar
-        selectedTab={selectedTab}
-        setselectedTab={setselectedTab}
-      ></Sidebar>
-      <div className="content">
-        <Header></Header>
-        {selectedTab == "Home" ? (
-          <PostList></PostList>
-        ) : (
-          <CreatePost></CreatePost>
-        )}
-        <Footer></Footer>
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setselectedTab={setselectedTab}
+        ></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab == "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
       </div>
-    </div>
+    </PostListProvider>
   );
 }
 
---
+++ social-media-app/src/store/post-list-store.jsx
@@ -0,0 +1,62 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Goint to mumbai",
+    body: "Hi friends, I am going to mumbai for my vacation. Hope to enjoy a lot. Peace out",
+    reaction: 2,
+    userId: "user_1",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "All india Rank 1",
+    body: "Got the All India Rank 1 in Exam. Hard to believe,",
+    reaction: 10,
+    userId: "user_2",
+    tags: ["Exam", "Toper", "Hard work"],
+  },
+];
+
+export default PostListProvider;
